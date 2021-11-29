493f333cc247b967493a9e25c23f6379b25102d7
TobyWans <62607977+TobyWans@users.noreply.github.com>
2021-11-29 16:44:31 +0000

    request menu

--- ui_layer/work_request_menu.py
+++ ui_layer/work_request_menu.py
@@ -1,7 +1,27 @@
+from ui_layer.login_checker import LogInCheck
+
+
 class WorkRequestMenu:
     def __init__(self):
-        self.options = """
-        
+        self.supervisor_options = """
+Work requests:
+        1. Create new request
+        2. Open/Change request
+        3. Close request
+        Q.Back
+        """
+        self.employee_options = """
+Work requests:
+        1. All work requests
+        2. Search by ID
+        3. Search by date
+        4. Your open requests
+        5. Finished request
+        Q. Back
         """
     
+    def draw_options(self):
+        if LogInCheck.ID_login(): print(self.supervisor_options)
+        else: print(self.employee_options)
+        
     
