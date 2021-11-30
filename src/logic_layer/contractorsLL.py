3432bab852383451d93f155bb6ee427521a95421
n1kki <89984817+n1kki@users.noreply.github.com>
2021-11-30 15:18:43 +0000

    contractor LL/UI push

---
+++ src/logic_layer/contractorsLL.py
@@ -0,0 +1,12 @@
+from LLAPI import LLAPI
+
+class ContractorMenuLL:
+    def __init__(self): # Þörf á init?
+        self.contractor = []
+        self.contractors_list = []
+
+    def add_new_contractor(self): # talar við DLAPI
+        pass
+
+    def list_all_contractors(self): # líka
+        pass
--- src/ui_layer/contractor_menu.py
+++ src/ui_layer/contractor_menu.py
@@ -1,25 +1,39 @@
+from src.logic_layer.LLAPI import LLAPI
 
 
 class ContractorMenu:
     def __init__(self):
-        self.supervisor_options = ["list imported from LLAPI", "Add new contractor", "Q. Back"]
-        self.employee_options = ["list imported from LLAPI", "Q. Back"]
-        self.is_supervisor
+        self.supervisor_options = ["list imported from LLAPI", "Add new contractor"]
+        self.employee_options = ["list imported from LLAPI"]
+        self.LLAPI = LLAPI()
+        self.LLAPI.ID_login("1234") # Place holder for testing. Veit ekki hvernig user id flakkast á milli klasa...
 
 
     def add_new_contractor(self):
-        pass
+        self.LLAPI.add_new_contractor()   #
 
     def list_all_contractors(self):
-        pass
+        self.LLAPI.list_all_contractors() # gera þessi föll í LLAPI?
 
     def draw_options(self):
-        pass
+        all_options = []
+        all_options.extend(self.employee_options)
+        if self.LLAPI.supervisor_check():
+            all_options.extend(self.supervisor_options)
+        for index in all_options:
+            print(f"\t{all_options.index(index) + 1}. {index}")
+        print("\tQ. Back\n")
 
     def prompt_input(self):
-        command = input("Enter number: ")
-        
-        if command == '1': pass
-        elif command == '2' and :
+        return_option = ''
+        while True:
+            command = input("Enter number: ")
+
+            if command == '1':
+                pass
+            elif command == '2':
+                pass
+            elif command.lower() == 'q':
+                return
         
         
