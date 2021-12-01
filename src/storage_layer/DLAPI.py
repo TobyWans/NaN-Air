c11249f0e11daa54bf7b10e59286746bd923a94a
valdidan <valdidanielsson@gmail.com>
2021-12-01 13:41:16 +0000

    Update DLAPI.py
    dlapi employee komið

--- src/storage_layer/DLAPI.py
+++ src/storage_layer/DLAPI.py
@@ -6,3 +6,18 @@ class DLAPI:
 
     def destination_info(self):
         return self.destDL.destination_info()
+
+    def create_new_employee(self):
+        pass
+
+    def change_employee(self):
+        pass
+
+    def get_all_employees(self):
+        pass
+
+    def search_employee_by_id(self):
+        pass
+
+    def search_employee_by_location(self):
+        pass
