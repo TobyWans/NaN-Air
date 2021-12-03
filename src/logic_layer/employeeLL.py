4ad8b6dc8769c26d384c0c3cea162bb66c61d39d
valdidan <valdidanielsson@gmail.com>
2021-12-03 11:03:00 +0000

    employee update

--- src/logic_layer/employeeLL.py
+++ src/logic_layer/employeeLL.py
@@ -9,18 +9,18 @@ class EmployeeLL:
     def all_employees(self):
         return self.dlapi.get_all_employees()
 
-    def create_new_employee(self, emp):
-        self.dlapi.create_new_employee(emp)
+    def create_new_employee(self):
+        self.dlapi.create_new_employee(self)
     
     def get_employees_that_started_working_before(self, date):
         pass
 
-    def change_employee(self, emp):
-        return self.dlapi.change_employee(emp)
+    def change_employee(self):
+        return self.dlapi.change_employee(self)
 
-    def search_employee_by_id(self, emp):
-        return self.dlapi.search_employee_by_id(emp)
+    def search_employee_by_id(self):
+        return self.dlapi.search_employee_by_id(self)
 
-    def serch_employee_by_location(self, emp):
-        return self.dlapi.search_employee_by_location(emp)
+    def serch_employee_by_location(self):
+        return self.dlapi.search_employee_by_location(self)
 
--- src/storage_layer/DLAPI.py
+++ src/storage_layer/DLAPI.py
@@ -47,13 +47,13 @@ class DLAPI:
         
         # Employee Logic
     def create_new_employee(self):
-        return self.employee.create_new_employee()
+        return self.employee.create_new_employee(self)
 
     def change_employee(self):
-        return self.employee.change_employee()
+        return self.employee.change_employee(self)
 
     def get_all_employees(self):
-        return self.employee.get_all_employees()
+        return self.employee.get_all_employees(self)
 
     def search_employee_by_id(self):
         return self.employee.search_employee_by_ID()
