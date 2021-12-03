2875b47b331c4aeba56c91169c557f011acfe01f
valdidan <valdidanielsson@gmail.com>
2021-12-03 12:23:02 +0000

    employee bug fixes

--- src/storage_layer/employeeDL.py
+++ src/storage_layer/employeeDL.py
@@ -11,13 +11,13 @@ class EmployeeDL:
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                emp = Employee(row["name"], row["email"],row["address"], row["phone"], row["mobile"], row["location"])
+                emp = Employee(row["name"], row["email"], row["address"], row["phone"], row["mobile"], row["location"])
                 ret_list.append(emp)
         return ret_list
 
     def create_new_employee(self, new_employee):
         with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ["name","email","address","phone","mobile", "location"]
+            fieldnames = ["name", "email", "address", "phone", "mobile", "location"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'name': new_employee.name, "email": new_employee.email, "address": new_employee.address, "phone": new_employee.phone, "mobile": new_employee.mobile, "location": new_employee.location})
        
