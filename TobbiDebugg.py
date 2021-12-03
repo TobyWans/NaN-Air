393f71b699029b1e82770299fd47926f38026502
TobyWans <62607977+TobyWans@users.noreply.github.com>
2021-12-03 17:21:55 +0000

    Create TobbiDebugg.py

---
+++ TobbiDebugg.py
@@ -0,0 +1,12 @@
+from src.logic_layer.LLAPI import LLAPI
+from src.ui_layer.main_menu import MainMenu
+
+
+
+
+
+if __name__ == "__main__":
+    menu = MainMenu()
+    menu.login()
+    # menu.draw_options()
+    menu.prompt_input()
