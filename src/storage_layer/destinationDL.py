b34c9490af70cf854fc0e351837177f6d4a10d02
soldis21 <soldis21@ru.is>
2021-12-08 13:07:48 +0000

    comment destination

--- src/storage_layer/destinationDL.py
+++ src/storage_layer/destinationDL.py
@@ -5,7 +5,7 @@ class DestinationDL:
     def __init__(self):
         self.filepath = "src/data/Destinations.csv"
 
-    def destination_info(self):
+    def destination_info(self):          # Reads the destination CSV file and returns a list of all the info.
         ret_list = []
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -14,7 +14,7 @@ class DestinationDL:
                 ret_list.append(dest)
         return ret_list
     
-    def get_only_city(self):
+    def get_only_city(self):          # Reads the destination CSV file and returns a list of only the cities.
         dest_list = []
         with open(self.filepath, newline='', encoding='utf-8') as destfile:
             reader = csv.reader(destfile)
@@ -23,12 +23,12 @@ class DestinationDL:
                 dest_list.append(row[0])
         return dest_list
     
-    def search_des_file_by_city(self, city):
+    def search_des_file_by_city(self, city):          # Checks if the city excists in the file.
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 if row['city'] == city:
                     des = Destination(**row)
-                    return des
-        return None
+                    return des          # if it does excist, it just returns the Destination class.
+        return None          # else it returns none.
 
