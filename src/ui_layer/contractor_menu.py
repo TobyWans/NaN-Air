24fff73bb7b463d8ef4577a1b3b6681486a8db11
n1kki <89984817+n1kki@users.noreply.github.com>
2021-12-10 18:10:53 +0000

    update contractor_menu.py

--- src/ui_layer/contractor_menu.py
+++ src/ui_layer/contractor_menu.py
@@ -22,87 +22,6 @@ _  /|  / / /_/ /_  / / /     _  ___ |  / _  /
                                                  """
 
 
-    def add_new_contractor(self):         # First instantiates all info into a model class then sends it all the way to the storage layer
-        self.llapi.clear_console()        # Also checks for correct format on all inputs
-
-        while True:                       # Location
-            location = input("Enter location (Svalbard/Nuuk/Faroe Islands): ")
-            if location.lower() != 'svalbard' and location.lower() != 'nuuk' and location.lower() != 'faroe islands':
-                print("Invalid location! Try again")
-            else:
-                break
-                                          # Contractor/company name
-        contractor = input("Enter contractor (company name): ")
-
-        name = None                       # Name
-        while name == None:
-            name = input("Enter name: ")
-            for letter in name:
-                if letter.isdigit():
-                    print("Input only letters!")
-                    name = None
-                    break
-
-        profession = None                 # Profession
-        while profession == None:
-            profession = input("Enter profession: ")
-            for letter in profession:
-                if letter.isdigit():
-                    print("Enter only letters!")
-                    profession = None
-                    break
-
-        phone_input = None                # Phone number
-        while phone_input == None:
-            try:
-                phone_input = int(input("Enter phone number (without dialling code): "))
-                if len(str(phone_input)) < 6:
-                    print("Phone number entered is too short! Try again")
-                    phone_input = None
-                if len(str(phone_input)) > 14:
-                    print("Phone number entered is too long! Try again")
-                    phone_input = None
-            except ValueError:
-                print("Enter only integers!")
-                phone_input =None
-
-        if location.lower() == 'svalbard': # Country code
-            phone = f'+47 {phone_input}'
-        elif location.lower() == 'nuuk':
-            phone = f'+299 {phone_input}'
-        elif location.lower() == 'faroe islands':
-            phone = f'+298 {phone_input}'
-
-        while True:                        # Opening hours
-            opening_hours = input("Enter opening hours (hh:mm-hh:mm): ")
-            try:
-                if int(opening_hours[0:2]) in range(0,60) and int(opening_hours[3:5]) in range(0,60) and int(opening_hours[6:8]) in range(0,60) and int(opening_hours[9:11]) in range(0,60):
-                    break
-                else:
-                    print("Invalid format!")
-            except ValueError:
-                print("Invalid value!") 
-
-        while True:                       # Rating
-            rating = input("Enter rating (X/10): ")
-            if rating.isnumeric():
-                if int(rating) <= 10 and int(rating) >= 0:
-                    break
-            else:
-                print("Invalid rating!")
-
-        confirm = ""                      # Confirm
-        while confirm != "y" and confirm != "n":
-            confirm = input("Confirm? (y/n): ")
-            if confirm.lower() =='y':
-                Contractor_mdl = contractors(contractor, name, profession, phone, opening_hours, location, rating)
-                return self.llapi.add_new_contractor(Contractor_mdl)
-            elif confirm.lower() == "n":
-                return
-            else:
-                print(INVALID)
-
-
     def draw_options(self):               # Displays all the available options for the user
         self.llapi.clear_console()
         print(self.splash_screen)
@@ -127,6 +46,9 @@ _  /|  / / /_/ /_  / / /     _  ___ |  / _  /
             command = input("\tEnter an option: ")
             if self.llapi.supervisor_check() and command == str(self.all_options.index("Add new contractor")+1):
                 self.add_new_contractor() # Supervisors can add a new contractor by entering the last number before the return option
+                self.llapi.clear_console()
+                print("\n\n\n\n\tContractor has been created. Refreshing list...")
+                time.sleep(1.8)
                 return
             elif command.lower() == 'r':
                 return
@@ -145,4 +67,85 @@ _  /|  / / /_/ /_  / / /     _  ___ |  / _  /
                     print("Invalid option, please try again.")
                     time.sleep(1)
         
-        
+
+    def add_new_contractor(self):         # First instantiates all info into a model class then sends it all the way to the storage layer
+        self.llapi.clear_console()        # Also checks for correct format on all inputs
+
+        while True:                       # Location
+            location = input("Enter location (Svalbard/Nuuk/Faroe Islands): ")
+            if location.lower() != 'svalbard' and location.lower() != 'nuuk' and location.lower() != 'faroe islands':
+                print("Invalid location! Try again")
+            else:
+                break
+                                          # Contractor/company name
+        contractor = input("Enter contractor (company name): ")
+
+        name = None                       # Name
+        while name == None:
+            name = input("Enter name: ")
+            for letter in name:
+                if letter.isdigit():
+                    print("Enter only letters!")
+                    name = None
+                    break
+
+        profession = None                 # Profession
+        while profession == None:
+            profession = input("Enter profession: ")
+            for letter in profession:
+                if letter.isdigit():
+                    print("Enter only letters!")
+                    profession = None
+                    break
+
+        phone_input = None                # Phone number
+        while phone_input == None:
+            try:
+                phone_input = int(input("Enter phone number (without dialling code): "))
+                if len(str(phone_input)) < 6:
+                    print("Phone number entered is too short!")
+                    phone_input = None
+                if len(str(phone_input)) > 14:
+                    print("Phone number entered is too long!")
+                    phone_input = None
+            except ValueError:
+                print("Enter only integers!")
+                phone_input =None
+
+        if location.lower() == 'svalbard': # Country code
+            phone = f'+47 {phone_input}'
+        elif location.lower() == 'nuuk':
+            phone = f'+299 {phone_input}'
+        elif location.lower() == 'faroe islands':
+            phone = f'+298 {phone_input}'
+
+        while True:                        # Opening hours
+            opening_hours = input("Enter opening hours (hh:mm-hh:mm): ")
+            try:
+                if int(opening_hours[0:2]) in range(00,24) and int(opening_hours[3:5]) in range(0,60) and int(opening_hours[6:8]) in range(00,24) and int(opening_hours[9:11]) in range(0,60):
+                    break
+                else:
+                    print("Invalid format!")
+            except ValueError:
+                print("Invalid value!") 
+
+        while True:                       # Rating
+            rating = input("Enter rating (X/10): ")
+            if rating.isnumeric():
+                if int(rating) <= 10 and int(rating) >= 0:
+                    break
+                else:
+                    print("Rating is out of range!")
+            else:
+                print("Invalid rating!")
+
+        confirm = ""                      # Confirm
+        while confirm != "y" and confirm != "n":
+            confirm = input("Confirm? (y/n): ")
+            if confirm.lower() =='y':
+                Contractor_mdl = contractors(contractor, name, profession, phone, opening_hours, location, rating)
+                return self.llapi.add_new_contractor(Contractor_mdl)
+            elif confirm.lower() == "n":
+                return
+            else:
+                print(INVALID)
